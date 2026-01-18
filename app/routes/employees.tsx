7a1fe9bbe666017505da6cd8fcad536f340324e0
Adi Zulfikar <adizulf@gmail.com>
2026-01-18 12:58:09 +0700

    refactor(employees): replace direct state management with useEmployees hook

--- app/routes/employees.tsx
+++ app/routes/employees.tsx
@@ -1,7 +1,7 @@
-import { useLoaderData, useSearchParams } from "react-router";
 import type { Route } from "./+types/employees";
 import { getEmployees } from "~/services/api";
 import { EmployeesView } from "~/views/EmployeesView";
+import { useEmployees } from "~/hooks/useEmployees";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const url = new URL(request.url);
@@ -14,15 +14,7 @@ export async function loader({ request }: Route.LoaderArgs) {
 }
 
 export default function Employees() {
-  const { employees, total, page, limit } = useLoaderData<typeof loader>();
-  const [_searchParams, setSearchParams] = useSearchParams();
-
-  const handlePageChange = (newPage: number) => {
-    setSearchParams((prev) => {
-      prev.set("_page", newPage.toString());
-      return prev;
-    });
-  };
+  const { employees, total, page, limit, handlePageChange } = useEmployees();
 
   return (
     <EmployeesView
